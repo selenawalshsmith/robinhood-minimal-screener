a0162c3214d617dfa80df6e8b549596598fbbe1a
Selena <selenawalshsmith@gmail.com>
2020-08-22 13:06:09 -0700

    Added finnhub api

--- src/components/showStocks.js
+++ src/components/showStocks.js
@@ -1,5 +1,6 @@
 import React, {Component}  from 'react';
 import axios from 'axios';
+const finnhub = require('finnhub');
 //import getTopList  from '../utils/getList.js';
 
 class ShowStocks extends Component{
@@ -8,106 +9,131 @@ class ShowStocks extends Component{
     this.state = {
       topList: [],
       symbols: [],
+      quotes: [],
+      count: 0
       //isLoading: true;
     };
-    //this.setState = this.setState.bind(this);
   }
 
-  loadStock = (url) => {
+  finnhubConfig = () => {
+    const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+    api_key.apiKey = "bsre93v48v6tucpg3cpg" // Replace this
+    const finnhubClient = new finnhub.DefaultApi();
+    console.dir(finnhubClient);
+  }
+  //var count = 0;
+  loadQuotes = (symbol) => {
+    //console.log(symbol);
+    //console.log(symbols);
+    const token = "bsre93v48v6tucpg3cpg"
+    const url = "https://finnhub.io/api/v1/quote?symbol="+symbol+"&token="+token;
     axios(url)
     .then((response) => {
-      const lst = [];
-      for(let i=0; i < response.data.length-1; i++){
-        lst.push(response.data.symbol);
-      }
-      this.setState({symbols: lst});
+      console.log(response);
+    }).catch(err => {console.log(err);});
+  }
+  //setInterval(function(){
+  //  this.loadQuotes()}, 500)
+  loadStocks = (url) => {
+    axios(url)
+    .then((response) => {
+      //const lst = [];
+      //console.log(response.data.symbol);
+      //for(let i=0; i < response.data.length-1; i++){
+        //console.log(response.data.symbol);
+        //lst.push(response.data.symbol);
+      this.setState({ symbols: [...this.state.symbols, response.data.symbol] });
+      //}
+      //console.log(lst);
+      //this.setState({symbols: lst});
+      //console.log(this.state.symbols);
+      //this.loadQuotes(this.state.symbols);
+      //console.log(this.state.symbols);
     }).catch(err => {console.log(err);});
     //return stock object.
+    //console.log(this.state.symbols);
   };
-  getStocks = () => {
+
+  loadStockURLs = () => {
+    const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+    api_key.apiKey = "bsre93v48v6tucpg3cpg" // Replace this
+    const finnhubClient = new finnhub.DefaultApi();
+    console.dir(finnhubClient);
     if (this.state.topList.length > 0 ){
       const lst = this.state.topList;
+      //console.log(this.state.topList);
+      const sym = [];
+      for(let i=0; i < lst.length-1; i++){
+        //console.log(response.data.symbol);
+        let path = lst[i].slice(25);
+        axios(path)
+        .then((response) => {
+          console.log(response.data.symbol);
+          /*
+          finnhubClient.quote(response.data.symbol, (error, data, response) => {
+            console.log(data)
+          });
+          */
+        }).catch(err => {console.log(err);});
+
+      }
+      /*
+      setInterval(() => {
+        for(let i=0; i < lst.length-1; i++){
+          let path = lst[i].slice(25);
+          //this.loadStocks(path);
+          axios(path)
+          .then((response) => {
+            //setInterval(this.loadQuotes(response.data.symbol), 9000000);
+            this.loadQuotes(response.data.symbol);
+          }).catch(err => {console.log(err);});
+          //console.log(this.state.count);
+          this.state.count++;
+        }
+      }, 50000);
+      /*
       for(let i=0; i < lst.length-1; i++){
         let path = lst[i].slice(25);
-        this.loadStock(path);
+        //this.loadStocks(path);
+        axios(path)
+        .then((response) => {
+          //setInterval(this.loadQuotes(response.data.symbol), 9000000);
+          this.loadQuotes(response.data.symbol);
+        }).catch(err => {console.log(err);});
+        //console.log(this.state.count);
+        this.state.count++;
       }
-    }
-  }
-/*
-  const fetchList = () => {
-    new Promise((resolve, reject) => {
-      resolve(() => { const topList = getTopList(); })
+      */
+      //console.dir(sym);
+      //console.dir(sym[0]);
+      //this.setState({symbols: sym});
+      //console.log(this.state.symbols.length);
+      //console.log(Object.keys(this.state.symbols));
 
     }
   }
-  */
-  /*
-  fetchList = () => {
-    getTopList();
-    new Promise((resolve, reject) => {
-      if (Array.isArray && lst.length > 0){
-        this.setState({topList: lst});
-        resolve(console.log(this.state.topList));
-      } else {
-        reject(console.log("LOADING RESOURCES...."));
-      }
-    })
-  }
-  */
-  getTopList = () => {
+  loadTopList = () => {
     const url = "midlands/tags/tag/100-most-popular/";
     axios(url)
     .then((response) => {
       //console.log(response.data.instruments[0]);
       //const lst = [];
       const lst = [...response.data.instruments];
-      /*
-      for(let i=0; i <= response.data.instruments.length -1; i++){
-        lst.push(response.data.instruments[i]);
-        console.log(lst);
-        //console.log(response.data.instruments[i]);
-        //this.setState({topList: lst})
-        //this.loadStock(response.data.instruments[i]);
-
-      }
-      */
-
-      //currentComponent.setState({topList: lst})
-      //console.log(lst);
-      //return lst;
-      //return response.data.instruments;
-      this.setState({topList: lst})
-      this.getStocks();
-      /*
-      new Promise((resolve, reject) => {
-        console.log("hello?");
-        //if (Array.isArray && lst.length > 0){
-          if (lst.length > 0){
-          resolve(()=>{console.log(this.state.topList); this.setState({topList: lst}); });
-        } else {
-          reject(console.log("LOADING RESOURCES...."));
-        }
-      })
-      */
+      this.setState({topList: lst});
+      this.loadStockURLs();
+      //setInterval(this.loadStockURLs,90000);
     })
     .catch(function (error) {
       console.log(error);
     });
   }
-
   componentDidMount(){
-    //const topList = getTopList();
-    //xo().then(console.log(topList));
-    /*
-    this.getTopList.then(() => {
-      for(let i=0; i<= this.state.topList.length; i++){
-        console.log(this.state.topList[i])
-      }
-    });
-    */
-    //const list = this.getTopList();
-    this.getTopList();
+    //this.finnhubConfig();
+    this.loadTopList();
 
+    if(this.state.symbols.length > 0){
+
+    }
   }
 
   render (){
@@ -119,6 +145,7 @@ class ShowStocks extends Component{
               <h2>Symbol</h2>
             </div>
           </div>
+          <button onClick={}>Show More</button>
         </div>
       </div>
     )
