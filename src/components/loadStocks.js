c0e5acc8aeddbed8f9823ada7f34e875f34b256e
Selena <selenawalshsmith@gmail.com>
2020-09-13 10:21:27 -0700

    implemented nested promises to ensure symbol  data stays in correct order

--- src/components/loadStocks.js
+++ src/components/loadStocks.js
@@ -1,9 +1,6 @@
 import React, {Component}  from 'react';
 import ShowStock from './showStock.js';
 import axios from 'axios';
-import finnhub from 'finnhub';
-
-//import getTopList  from '../utils/getList.js';
 
 class LoadStocks extends Component{
   constructor(props) {
@@ -15,94 +12,41 @@ class LoadStocks extends Component{
       isLoading: true
     };
   }
-  /*
-  loadQuotes = (arr) => {
-    //console.log(symbol);
-    //console.log(symbols);
-    //console.log(arr); still in order here
-    const quotelst = [];
-    //if (arr.length === 25){
-      const token = "bsre93v48v6tucpg3cpg"
-      for (let i=0; i < arr.length; i++){
-        let symbol = arr[i];
-        let url = "https://finnhub.io/api/v1/quote?symbol="+symbol+"&token="+token;
-          axios(url)
-          .then((response) => {
-            //console.log(response);
-            //console.log(i);
-            const quoteItem = {
-              sym:        symbol,
-              cost:       response.data.c,
-              high:       response.data.h,
-              low:        response.data.l,
-              open:       response.data.o,
-              prev_close: response.data.pc,
-            }
-            //console.log(quoteItem);
-            quotelst.push(quoteItem);
-            this.setState({ quotes: [...this.state.quotes, quotelst] });
-            //quotelst[quotelst.length] = quoteItem;
-          }).catch(err => {console.log(err);});
-      }
-      console.log(this.state.quotes);
-      this.setState({isLoading: false});
-  }
-  */
-  /*
-  loadFirstStocks = () => {
-    if (this.state.symbols.length === 100){
-      var arr = this.state.symbols.slice(0, 30);
-      this.loadQuotes(arr);
-    }
-  };
-  */
-
   loadSymbols = () => {
-    if (this.state.topList.length === 100 ){
-      const lst = this.state.topList;
-      const sym = [];
-      for(let i=0; i < lst.length; i++){
-        let path = lst[i].slice(25);
-        axios(path)
-        .then((response) => {
-          this.setState({ symbols: [...this.state.symbols, response.data.symbol] });
-          //this.loadFirstStocks();
-          //console.log(this.state.symbols.length);
-          if(this.state.symbols.length === 100){
-            console.log(this.state.symbols);
-            this.setState({isLoading: false});
-          }
-        }).catch(err => {console.log(err);});
+    var sym = [];
+    Promise.all(this.state.topList.map(u=>
+      fetch(u.slice(25))
+    )).then(responses =>
+      Promise.all(responses.map(res => res.json()))
+    ).then(json => {
+      for(let i=0; i < json.length; i++){
+        sym.push(json[i].symbol);
       }
-      //this.loadFirstStocks();
-    }
+      if (sym.length === 100){
+        this.setState({ symbols: sym });
+        this.setState({isLoading: false});
+      }
+    });
   }
   loadTopListURLs = () => {
     const url = "midlands/tags/tag/100-most-popular/";
+    var lst = [];
     axios(url)
     .then((response) => {
-      //Log statement below is list of robinhood urls for each top stock
-      //console.log(response.data.instruments[5]);
-      //const lst = [];
-      const lst = [...response.data.instruments];
+      response.data.instruments.map((instPath) => {
+        lst.push(instPath);
+      });
       this.setState({topList: lst});
       this.loadSymbols();
-      //this.loadStockURLs();
-      //setInterval(this.loadStockURLs,90000);
     })
     .catch(function (error) {
       console.log(error);
     });
   }
-  handleClick = () => {
-    console.log("clicked");
-  }
   componentDidMount(){
     //this.finnhubConfig();
     this.loadTopListURLs();
-
   }
-
   render (){
     return (
       <div>
